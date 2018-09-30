f67c1d8e79ca127919218818afe475f9e8ea2bca
jerrylizilong <lizion138@163.com>
2018-09-30 09:50:24 +0800

    处理截图等方法

--- src/main/java/jerry_selenium_demo1/JerrySelenium.java
+++ src/main/java/jerry_selenium_demo1/JerrySelenium.java
@@ -1,27 +1,51 @@
 package jerry_selenium_demo1;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.NoSuchElementException;
 
 public class JerrySelenium {
-    WebDriver driver;
     public static void main(String [] args){
 
     }
 
-    public static void saveScreenshot(WebDriver driver) throws IOException {
+        // 初始化driver： 根据传入的浏览器类型初始化对应的浏览器驱动
+    public static WebDriver initDriver(String browserType) throws MalformedURLException {
+        WebDriver driver;
+        // 调用本地driver
+//        driver = new ChromeDriver();
+
+        // 调用远程 driver
+        DesiredCapabilities driverOption;
+        if ("Firefox".equals(browserType)){
+            driverOption = DesiredCapabilities.firefox();
+        }else {
+            driverOption= DesiredCapabilities.chrome();
+        }
+
+        driver = new RemoteWebDriver(new URL("http://172.16.100.168:4444/wd/hub/"),driverOption);
+        driver.manage().window().setSize(new Dimension(1920,1080));
+        return driver;
+    }
+
+        // 保存截图
+    public static void saveScreenshot(WebDriver driver,String name) throws IOException {
         File pic = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         SimpleDateFormat currentTime = new SimpleDateFormat("yyyyMMdd-HHmmss");
         String time = currentTime.format(new Date());
-        FileUtils.copyFile(pic, new File("test" + File.separator + time + ".jpg"));
+        FileUtils.copyFile(pic, new File("test" + File.separator+name +"-"+ time + ".jpg"));
     }
 
     public static boolean assertPageTitle(WebDriver driver,String target) throws NoSuchElementException{
--- src/test/java/jerry_selenium_demo1/JerryTestngSelenium.java
+++ src/test/java/jerry_selenium_demo1/JerryTestngSelenium.java
@@ -1,9 +1,7 @@
 package jerry_selenium_demo1;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,41 +11,39 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 
+
 public class JerryTestngSelenium {
     WebDriver driver ;
 
     @BeforeMethod
     public void beforClass() throws MalformedURLException {
         System.out.printf("\nthis is a before class");
-
-        // 调用本地driver
-        driver = new ChromeDriver();
-
-        // 调用远程 driver
-//        DesiredCapabilities chromeOption= DesiredCapabilities.chrome();
-//        driver = new RemoteWebDriver(new URL("http://172.16.100.168:4444/wd/hub/"),chromeOption);
-
-        driver.manage().window().setSize(new Dimension(1920,1080));
+        driver = JerrySelenium.initDriver("Chrome");
         driver.get("http://www.baidu.com/");
 
     }
 
     @Test
-    public void testBaiduNews(){
+    public void testBaiduNews() throws IOException {
         driver.findElement(By.partialLinkText("新闻")).click();
-//        Assert.assertTrue(driver.getTitle().contains("百度新闻"));
+        String methodName = String.valueOf(Thread.currentThread().getStackTrace()[1].getMethodName());
+        JerrySelenium.saveScreenshot(driver,methodName);
         Assert.assertTrue(JerrySelenium.assertPageTitle(driver,"百度新闻"));
     }
 
     @Test
-    public void testBaiduMap(){
+    public void testBaiduMap() throws IOException {
         driver.findElement(By.partialLinkText("地图")).click();
+        String methodName = String.valueOf(Thread.currentThread().getStackTrace()[1].getMethodName());
+        JerrySelenium.saveScreenshot(driver,methodName);
         Assert.assertTrue(JerrySelenium.assertPageTitle(driver, "百度地图"));
     }
 
     @Test
-    public void testBaiduVedio(){
+    public void testBaiduVedio() throws IOException {
         driver.findElement(By.partialLinkText("视频")).click();
+        String methodName = String.valueOf(Thread.currentThread().getStackTrace()[1].getMethodName());
+        JerrySelenium.saveScreenshot(driver,methodName);
         Assert.assertTrue(JerrySelenium.assertPageTitle(driver, "百度视频"));
     }
 
@@ -55,8 +51,6 @@ public class JerryTestngSelenium {
     public void afterClass() throws IOException {
         System.out.printf("\nthis is after class\n");
         System.out.printf(driver.getCurrentUrl());
-        // 保存截图
-        JerrySelenium.saveScreenshot(driver);
         driver.close();
     }
 }
